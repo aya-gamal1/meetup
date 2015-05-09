347b0e8581145fddc4ed6f692492f8234f0c5317
mona-os <amatto_allah13@yahoo.com>
2015-05-09 12:46:45 +0200

    sign_up&login&sendmail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,34 @@
 class User < ActiveRecord::Base
-	has_many :events, :through => :eventadmins
+
+
+
+
+
+
+  attr_accessor :password,  :email, :name, :gender, :age, :city, :country, :avatar 
+
+
+
+validates :name, :password, :email, :gender, :age, :avatar, :country, :city , presence: true
+  validates :password, length: { in: 6..20 }
+  validates :email, length: { minimum: 6 } ,uniqueness: true
+  validates :age, numericality: { only_integer: true }
+
+
+def self.authenticate(email, password)
+    user = find_by_email(email)
+    user.password = find_by_password(password)
+    if user && user.password
+      user
+    else
+      nil
+    end
+  end
+
+
+
+
+has_many :events, :through => :eventadmins
 	has_many :eventadmins
 	has_many :comments
 	has_many :groups
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506212045) do
+ActiveRecord::Schema.define(version: 20150509094509) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text",       limit: 65535
@@ -87,17 +87,21 @@ ActiveRecord::Schema.define(version: 20150506212045) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
-    t.string   "password",   limit: 255
-    t.string   "gender",     limit: 255
-    t.integer  "age",        limit: 4
-    t.string   "city",       limit: 255
-    t.string   "country",    limit: 255
-    t.string   "avatar",     limit: 255
-    t.string   "isadmin",    limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name",                limit: 255
+    t.string   "email",               limit: 255
+    t.string   "password",            limit: 255
+    t.string   "gender",              limit: 255
+    t.integer  "age",                 limit: 4
+    t.string   "city",                limit: 255
+    t.string   "country",             limit: 255
+    t.string   "avatar",              limit: 255
+    t.string   "isadmin",             limit: 255
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "avatar_file_name",    limit: 255
+    t.string   "avatar_content_type", limit: 255
+    t.integer  "avatar_file_size",    limit: 4
+    t.datetime "avatar_updated_at"
   end
 
   add_foreign_key "comments", "events"
