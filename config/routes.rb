b0dcff447090241d0ac8090da9eaa9105fce5e4a
aya-gamal1 <eng.aya_gamal@outlook.com>
2015-05-11 21:51:03 +0300

    fix conflict and add tag to group

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ resources :users
 get "log_out" => "sessions#destroy", :as => "log_out"
 get "log_in" => "sessions#new", :as => "log_in"
 get "sign_up" => "users#new", :as => "sign_up"
-<<<<<<< HEAD
+
 #root :to => "users#index"
 resources :sessions
 root :to => "groups#index"
